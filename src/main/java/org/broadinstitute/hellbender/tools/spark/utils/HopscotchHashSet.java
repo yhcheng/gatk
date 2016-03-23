44fe3241b4a3b3735e0b22f064ee4ada7b1a30b1
tsharpe <tsharpe@broadinstitute.org>
2016-03-23 17:59:01 -0400

    better eviction algo

--- src/main/java/org/broadinstitute/hellbender/tools/spark/utils/HopscotchHashSet.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/utils/HopscotchHashSet.java
@@ -16,10 +16,10 @@ import java.util.NoSuchElementException;
 public final class HopscotchHashSet<T> extends AbstractSet<T> implements Serializable {
     private static final long serialVersionUID = 1L;
     private int size;
-    private final int capacity;
+    private int capacity;
     // unused buckets contain null.  (this data structure does not support null entries.)
     // if the bucket is unused, the corresponding status byte is irrelevant, but is always set to 0.
-    private final T[] buckets;
+    private T[] buckets;
     // format of the status bytes:
     // high bit set indicates that the bucket contains "chain head" (i.e., an entry that naturally belongs in the
     // corresponding bucket).  high bit not set indicates a "squatter" (i.e., an entry that got placed here through the
@@ -27,32 +27,31 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
     // low 7 bits give the (unsigned) offset from the current entry to the next entry in the collision resolution chain.
     // if the low 7 bits are 0, then we'd be pointing at ourselves, which is nonsense, so that particular value marks
     // "end of chain" instead.  we use Byte.MAX_VALUE (i.e., 0x7f) to pick off these bits.
-    private final byte[] status;
+    private byte[] status;
 
-    @VisibleForTesting static final double LOAD_FACTOR = .75;
+    @VisibleForTesting static final double LOAD_FACTOR = .85;
 
-    // largest prime numbers less than increasing powers of 2 (from 8 to 32)
-    private static final int[] legalSizes = {
-        (1<<8)-5, (1<<9)-3, (1<<10)-3, (1<<11)-9, (1<<12)-3, (1<<13)-1, (1<<14)-3, (1<<15)-19, (1<<16)-15, (1<<17)-1,
-        (1<<18)-5, (1<<19)-1, (1<<20)-3, (1<<21)-9, (1<<22)-3, (1<<23)-15, (1<<24)-3, (1<<25)-39, (1<<26)-5,
-        (1<<27)-39, (1<<28)-57, (1<<29)-3, (1<<30)-35, Integer.MAX_VALUE
+    // largest prime numbers less than each half power of 2 from 2^8 to 2^31
+    @VisibleForTesting static final int[] legalSizes = {
+            251, 359, 509, 719, 1021, 1447, 2039, 2887, 4093, 5791, 8191, 11579, 16381, 23167, 32749, 46337, 65521,
+            92681, 131071, 185363, 262139, 370723, 524287, 741431, 1048573, 1482907, 2097143, 2965819, 4194301, 5931641,
+            8388593, 11863279, 16777213, 23726561, 33554393, 47453111, 67108859, 94906249, 134217689, 189812507,
+            268435399, 379625047, 536870909, 759250111, 1073741789, 1518500213, 2147483647
     };
 
-    @SuppressWarnings("unchecked")
     @VisibleForTesting HopscotchHashSet( final int capacity ) {
-        this.size = 0;
-        this.capacity = capacity;
-        // Not unsafe, because the remainder of the API allows only elements known to be T's to be assigned to buckets.
-        // In fact, except for the testing interface, this class is an immutable loaded by a collection of T's.
-        this.buckets = (T[])new Object[capacity];
-        this.status = new byte[capacity];
+        init(capacity);
     }
 
+    /**
+     * Make a HashSet from the provided collection.  N.B.: Null entries are ignored.
+     */
     public HopscotchHashSet( final Collection<T> entries ) {
-        this(computeCapacity(entries.size()));
-        for ( final T entry : entries ) {
-            if ( entry == null ) throw new UnsupportedOperationException("This type of set can't hold null values.");
-            insert(entry);
+        init(computeCapacity(entries.size()));
+        if ( !insertAll(entries) ) {
+            init(bumpCapacity(capacity));
+            if ( !insertAll(entries) )
+                throw new IllegalStateException("Unable to build hashSet.  Maybe your hash function is screwy?");
         }
     }
 
@@ -74,25 +73,32 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
 
     public Iterator<T> bucketIterator( final int hashVal ) { return new BucketIterator(hashVal); }
 
+    private boolean insertAll( final Collection<T> entries ) {
+        try {
+            for ( final T entry : entries ) {
+                if ( entry != null ) insert(entry);
+            }
+        } catch ( final IllegalStateException ise ) {
+            return false;
+        }
+        return true;
+    }
 
     @VisibleForTesting boolean insert( final T entry ) {
         final int bucketIndex = hashToIndex(entry.hashCode());
-        // if the place where it should go is empty, just put the new entry there
-        if ( buckets[bucketIndex] == null ) {
-            emplaceChainHead(bucketIndex, entry);
-            size += 1;
-            return true;
-        }
 
         // if there's a squatter where the new entry should go, move it elsewhere and put the entry there
-        if ( !isChainHead(bucketIndex) ) {
-            evict(bucketIndex);
-            emplaceChainHead(bucketIndex, entry);
+        if ( buckets[bucketIndex] != null && !isChainHead(bucketIndex) ) evict(bucketIndex);
+
+        // if the place where it should go is empty, just put the new entry there
+        if ( buckets[bucketIndex] == null ) {
+            buckets[bucketIndex] = entry;
+            status[bucketIndex] = Byte.MIN_VALUE;
             size += 1;
             return true;
         }
 
-        // find the end of the chain of entries for this bucket, making sure the entry isn't already present
+        // make sure the entry isn't already present
         int endOfChainIndex = bucketIndex;
         while ( true ) {
             // if entry is already in the set
@@ -102,21 +108,11 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
             endOfChainIndex = getIndex(endOfChainIndex, offset);
         }
 
-        // find an empty bucket for the new entry
-        int emptyBucketIndex = findEmptyBucket(endOfChainIndex);
-
-        // hopscotch the empty bucket into range if it's too far away to point to from endOfChainIndex
-        int offset;
-        while ( (offset = getIndexDiff(endOfChainIndex, emptyBucketIndex)) > Byte.MAX_VALUE ) {
-            emptyBucketIndex = hopscotch(endOfChainIndex, emptyBucketIndex);
-        }
+        // find a place for the new entry
+        final int emptyBucketIndex = insertIntoChain(bucketIndex, endOfChainIndex);
 
         // put the new entry into the empty bucket
-        emplaceEndOfChain(emptyBucketIndex, entry);
-
-        // link the current end of chain to the new end of chain
-        // (we can add the offset because we know the current offset is zero:  it's a chain end.)
-        status[endOfChainIndex] += offset;
+        buckets[emptyBucketIndex] = entry;
         size += 1;
         return true;
     }
@@ -125,32 +121,70 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
         return Math.floorMod(hashVal, capacity);
     }
 
-    private void evict( final int bucketToEvictIndex ) {
-        int offsetToEvictee = 0;
-        int bucketIndex = bucketToEvictIndex;
-        while ( ++offsetToEvictee <= Byte.MAX_VALUE ) {
-            bucketIndex = getIndex(bucketIndex, -1);
-            if ( getOffset(bucketIndex) == offsetToEvictee ) {
-                int emptyBucketIndex = findEmptyBucket(bucketToEvictIndex);
-                while ( getIndexDiff(bucketIndex, emptyBucketIndex) > Byte.MAX_VALUE ) {
-                    //TODO: could be more aggressive.  it might be possible to have the empty bucket between bucketIndex and bucketToEvictIndex
-                    emptyBucketIndex = hopscotch(bucketToEvictIndex, emptyBucketIndex);
-                }
-                move(bucketIndex, bucketToEvictIndex, emptyBucketIndex);
-                return;
-            }
+    private int insertIntoChain( final int bucketIndex, final int endOfChainIndex ) {
+        final int offsetToEndOfChain = getIndexDiff(bucketIndex, endOfChainIndex);
+
+        // find an empty bucket for the new entry
+        int emptyBucketIndex = findEmptyBucket(bucketIndex);
+
+        // if the distance to the empty bucket is larger than this, we'll have to hopscotch
+        final int maxOffset = offsetToEndOfChain + Byte.MAX_VALUE;
+
+        // hopscotch the empty bucket into range if it's too far away
+        int offsetToEmpty;
+        while ( (offsetToEmpty = getIndexDiff(bucketIndex, emptyBucketIndex)) > maxOffset ) {
+            emptyBucketIndex = hopscotch(bucketIndex, emptyBucketIndex);
         }
-        throw new IllegalStateException("Can't find predecessor.");
+
+        // if the new entry lies downstream of the current chain end, just link it in
+        if ( offsetToEmpty > offsetToEndOfChain ) {
+            status[endOfChainIndex] += offsetToEmpty - offsetToEndOfChain;
+        } else {
+            linkIntoChain(bucketIndex, emptyBucketIndex);
+        }
+
+        return emptyBucketIndex;
     }
 
-    private void emplaceChainHead( final int bucketIndex, final T entry ) {
-        buckets[bucketIndex] = entry;
-        status[bucketIndex] = Byte.MIN_VALUE;
+    // walk the chain until we find where the new slot gets linked in
+    private void linkIntoChain( final int bucketIndex, final int emptyBucketIndex ) {
+        int offsetToEmpty = getIndexDiff(bucketIndex, emptyBucketIndex);
+        int tmpIndex = bucketIndex;
+        int offset;
+        while ( (offset = getOffset(tmpIndex)) < offsetToEmpty ) {
+            tmpIndex = getIndex(tmpIndex, offset);
+            offsetToEmpty -= offset;
+        }
+        offset -= offsetToEmpty;
+        status[tmpIndex] -= offset;
+        status[emptyBucketIndex] = (byte) offset;
     }
 
-    private void emplaceEndOfChain( final int bucketIndex, final T entry ) {
-        buckets[bucketIndex] = entry;
-        status[bucketIndex] = 0;
+    private void evict( final int bucketToEvictIndex ) {
+        final int bucketIndex = hashToIndex(buckets[bucketToEvictIndex].hashCode());
+        final int offsetToEvictee = getIndexDiff(bucketIndex, bucketToEvictIndex);
+        int emptyBucketIndex = findEmptyBucket(bucketIndex);
+        int fromIndex = bucketIndex;
+        while ( true ) {
+            while ( getIndexDiff(bucketIndex, emptyBucketIndex) > offsetToEvictee ) {
+                emptyBucketIndex = hopscotch(fromIndex, emptyBucketIndex);
+            }
+            if ( emptyBucketIndex == bucketToEvictIndex ) return;
+            fromIndex = emptyBucketIndex;
+            linkIntoChain(bucketIndex, emptyBucketIndex);
+            int prevIndex = bucketIndex;
+            int offsetToNext = getOffset(prevIndex);
+            int nextIndex = getIndex(prevIndex, offsetToNext);
+            while ( (offsetToNext = getOffset(nextIndex)) != 0 ) {
+                prevIndex = nextIndex;
+                nextIndex = getIndex(nextIndex, offsetToNext);
+            }
+            buckets[emptyBucketIndex] = buckets[nextIndex];
+            buckets[nextIndex] = null;
+            status[nextIndex] = 0;
+            status[prevIndex] -= getOffset(prevIndex);
+            emptyBucketIndex = nextIndex;
+        }
     }
 
     private int findEmptyBucket( int bucketIndex ) {
@@ -177,25 +211,28 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
         return result;
     }
 
+    // bucket1 is assumed to be upstream of bucket2 (even if bucket2's index has wrapped)
+    // i.e., the result is always positive
     private int getIndexDiff( final int bucketIndex1, final int bucketIndex2 ) {
         int result = bucketIndex2 - bucketIndex1;
         if ( result < 0 ) result += capacity;
         return result;
     }
 
-    private int hopscotch( final int endIndex, final int emptyBucketIndex ) {
-        int offsetToEmpty = 0;
-        int bucketIndex = emptyBucketIndex;
-        while ( ++offsetToEmpty <= Byte.MAX_VALUE ) {
-            bucketIndex = getIndex(bucketIndex, -1);
-            if ( bucketIndex == endIndex ) break;
+    private int hopscotch( final int fromIndex, final int emptyBucketIndex ) {
+        final int fromToEmptyDistance = getIndexDiff(fromIndex, emptyBucketIndex);
+        int offsetToEmpty = Byte.MAX_VALUE;
+        while ( offsetToEmpty > 1 ) {
+            final int bucketIndex = getIndex(emptyBucketIndex, -offsetToEmpty);
             final int offsetInBucket = getOffset(bucketIndex);
-            // if the bucketIndex bucket points to something closer than the empty bucket
-            if ( offsetInBucket != 0 && offsetInBucket < offsetToEmpty ) {
+            if ( offsetInBucket != 0 &&
+                    offsetInBucket < offsetToEmpty &&
+                    offsetToEmpty-offsetInBucket < fromToEmptyDistance ) {
                 final int bucketToMoveIndex = getIndex(bucketIndex, offsetInBucket);
-                move( bucketIndex, bucketToMoveIndex, emptyBucketIndex );
+                move(bucketIndex, bucketToMoveIndex, emptyBucketIndex);
                 return bucketToMoveIndex;
             }
+            offsetToEmpty -= 1;
         }
         throw new IllegalStateException("Hopscotching failed at load factor "+(1.*size/capacity));
     }
@@ -223,6 +260,16 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
         status[bucketToMoveIndex] = 0;
     }
 
+    @SuppressWarnings("unchecked")
+    private void init( final int capacity ) {
+        size = 0;
+        this.capacity = capacity;
+        // Not unsafe, because the remainder of the API allows only elements known to be T's to be assigned to buckets.
+        // In fact, except for the testing interface, this class is an immutable loaded by a collection of T's.
+        buckets = (T[])new Object[capacity];
+        status = new byte[capacity];
+    }
+
     private static int computeCapacity( final int size ) {
         if ( size < LOAD_FACTOR*Integer.MAX_VALUE ) {
             final int augmentedSize = (int) (size / LOAD_FACTOR);
@@ -231,6 +278,15 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
             }
         }
         return legalSizes[legalSizes.length-1];
+    }
+
+    private static int bumpCapacity( final int capacity ) {
+        if ( capacity <= legalSizes[legalSizes.length-2] ) {
+            for ( int idx = 0; idx != legalSizes.length; ++idx ) {
+                if ( legalSizes[idx] >= capacity ) return legalSizes[idx + 1];
+            }
+        }
+        throw new IllegalStateException("Unable to increase capacity.");
     }
 
     private final class BucketIterator implements Iterator<T> {
@@ -242,6 +298,7 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
         }
 
         @Override public boolean hasNext() { return bucketIndex != buckets.length; }
+
         @Override public T next() {
             if ( bucketIndex == buckets.length ) throw new NoSuchElementException("HopscotchHashSet iterator is exhausted.");
             final T result = buckets[bucketIndex];
@@ -258,6 +315,7 @@ public final class HopscotchHashSet<T> extends AbstractSet<T> implements Seriali
         CompleteIterator( final T[] buckets ) { this.buckets = buckets; this.bucketIndex = findNext(0); }
 
         @Override public boolean hasNext() { return bucketIndex != buckets.length; }
+
         @Override public T next() {
             if ( bucketIndex == buckets.length ) throw new NoSuchElementException("HopscotchHashSet iterator is exhausted.");
             final T result = buckets[bucketIndex];
--- src/test/java/org/broadinstitute/hellbender/tools/spark/utils/HopscotchHashSetTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/spark/utils/HopscotchHashSetTest.java
@@ -11,9 +11,34 @@ import java.util.Random;
  * Unit tests for HopscotchHashSet.
  */
 public class HopscotchHashSetTest extends BaseTest {
-    private static final int HHASH_CAPACITY = 8191;
-    private static final int HHASH_NVALS = HHASH_CAPACITY+1;//(int)(HopscotchHashSet.LOAD_FACTOR*HHASH_CAPACITY);
+    private static final int HHASH_CAPACITY = HopscotchHashSet.legalSizes[10];
+    private static final int HHASH_NVALS = (int)(HopscotchHashSet.LOAD_FACTOR*HHASH_CAPACITY);
     private static final int N_TRIALS = 10000;
+
+    @Test(groups = "spark")
+    void legalCapacitiesTest() {
+        final int[] caps = HopscotchHashSet.legalSizes;
+        final int nCaps = caps.length;
+        // test that they're spaced properly -- each is supposed to be about sqrt(2) bigger than the previous one
+        for ( int idx = 1; idx < nCaps; ++idx ) {
+            final double err = Math.abs(1. - Math.sqrt(2.)*caps[idx-1]/caps[idx]);
+            Assert.assertTrue(err < .015, "testing capacity "+caps[idx]+" at index "+idx);
+        }
+        // test that they're all primes
+        for ( int idx = 0; idx < nCaps; ++idx ) {
+            Assert.assertTrue(isPrime(caps[idx]), "testing capacity "+caps[idx]+" at index "+idx);
+        }
+    }
+
+    private static boolean isPrime( final long iii ) {
+        if ( iii % 2 == 0 || iii %3 == 0 ) return false;
+        long iFact = 5;
+        while ( iFact*iFact <= iii ) {
+            if ( iii % iFact == 0 || iii % (iFact+2) == 0 ) return false;
+            iFact += 6;
+        }
+        return true;
+    }
 
     @Test(groups = "spark")
     void loadRandomIntsTest() {
@@ -25,8 +50,8 @@ public class HopscotchHashSetTest extends BaseTest {
             for ( int valNo = 0; valNo != HHASH_NVALS; ++valNo ) {
                 final Integer randVal = rng.nextInt();
                 try { hhashSet.insert(randVal); } catch ( final IllegalStateException ise ) {
-                    final String msg = trialMsg + ", valNo=" + valNo + ", addedVal=" + randVal;
-                    System.out.println(ise.getMessage() + " " + msg);
+                    final String msg = " " + trialMsg + ", valNo=" + valNo + ", addedVal=" + randVal;
+                    System.out.println(ise.getMessage() + msg);
                     break;
                 }
                 hashSet.add(randVal);
